3e6a6d43fe6e66e6da4864f935f98acd04331896
张兆玉 <unknown@example.com>
2021-08-05 09:41:47 +0800

    增加页面
    增加了收藏和弹幕

--- pages/collect/collect.js
+++ pages/collect/collect.js
@@ -1,24 +1,27 @@
-// pages/cloud/cloud.js
+// pages/collect/collect.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    isLogin:true,
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    menuitems: [
-      { text: '我的足迹', url: './footPrint/ftprint', tips: '' },
-      { text: '我的收藏', url: '../collect/collect',  tips: '' },
-      { text: '个人信息', url: './yours/yours', tips: '' },
-      { text: '意见反馈', url: './fankui/fankui',  tips: '' }
-    ],
+    jingdian:[{
+      imgsrc:"https://pic.imgdb.cn/item/60fa4c845132923bf8e04e55.jpg",
+      title:"大明湖",
+      value:"济南三大历史名胜之一，湖水水色澄碧，是国家5A级旅游景区——天下第一泉风景区的核心组成部分之一。"
+    },{
+      imgsrc:"https://pic.imgdb.cn/item/60fa652f5132923bf82b7299.jpg",
+      title:"千佛山",
+      value:"千佛山古称历山；相传上古虞舜帝为民时，曾躬耕于历山之下，故亦名舜山或舜耕山。据史载：隋朝年间，山东佛教盛行，虔诚的教徒依山沿壁镌刻了为数较多的石佛，建千佛寺而得名千佛山。"
+    }],
+    tongzhi:[{
+      key:"第一条通知"
+    },{
+      key:"第二条通知"
+    },{
+      key:"第三条通知"
+    }]
   },
-onGotUserInfo: function(event){
-  console.log(event);
-},
 
   /**
    * 生命周期函数--监听页面加载
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,7 @@
 // pages/Demo/Demo.js
 Page({
   data: {
-    img:[{
+    img: {url: [{
       imgsrc:"https://pic.imgdb.cn/item/60fa1b5e5132923bf85bf037.png"
     },
     {
@@ -9,28 +9,39 @@ Page({
     },
     {
       imgsrc:"https://pic.imgdb.cn/item/60f94e365132923bf83636b9.jpg"
-    }],
-    tongzhi:"通知》》》》",
-    jingdian:[{
+    }]},
+    jingdian: {data:[{
       imgsrc:"https://pic.imgdb.cn/item/60fa4c845132923bf8e04e55.jpg",
       title:"大明湖",
       value:"济南三大历史名胜之一，湖水水色澄碧，是国家5A级旅游景区——天下第一泉风景区的核心组成部分之一。"
     },{
-      url:"../posts/posts",
       imgsrc:"https://pic.imgdb.cn/item/60fa652f5132923bf82b7299.jpg",
       title:"千佛山",
       value:"千佛山古称历山；相传上古虞舜帝为民时，曾躬耕于历山之下，故亦名舜山或舜耕山。据史载：隋朝年间，山东佛教盛行，虔诚的教徒依山沿壁镌刻了为数较多的石佛，建千佛寺而得名千佛山。"
-    },{
-      imgsrc:"https://pic.imgdb.cn/item/60fa652f5132923bf82b7299.jpg",
-      title:"千佛山",
-      value:"千佛山古称历山；相传上古虞舜帝为民时，曾躬耕于历山之下，故亦名舜山或舜耕山。据史载：隋朝年间，山东佛教盛行，虔诚的教徒依山沿壁镌刻了为数较多的石佛，建千佛寺而得名千佛山。"
-    }],
-    tongzhi:[{
-      key:"第一条通知"
-    },{
-      key:"第二条通知"
-    },{
-      key:"第三条通知"
-    }],  
-},
+    }]},
+    tongzhi: {key: ["第一条通知","第二条通知","第三条通知"]},
+    danmu:{data: [
+      {text: "这个小程序太好用了！",user: "张三"},
+      {text: "大明湖yyds!!!!!!!!!",user: "李四"},
+      {text: "千佛山偶遇走起",user: "王五"}
+    ],
+    time:[3000,2500,3500]}
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var thit = this;
+    // wx.request({
+    //   url: 'http://localhost:8080/Maven_Web/servletTest',
+    //   data: {
+    //     a:"1",
+    //     b:"2"
+    //   },
+    //   method:"GET",
+    //   success(res){
+    //     console.log(res.data);
+    //   }
+    // })
+  },
 })
